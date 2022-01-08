f6dfd3f53d5544e12401d229915008b26690e209
simply-ashtyn <97042764+simply-ashtyn@users.noreply.github.com>
2022-01-08 11:40:43 -0600

    Add files via upload

---
+++ LectureProgramPractice/LectureProgramPractice/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LectureProgramPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                //string gmsg = GetMessage();
+                //PrintMessage(gmsg);
+                //TimeStamp(ref gmsg);
+                //Console.WriteLine(gmsg);
+
+                MyFavoriteNumber();
+
+
+                Console.WriteLine("\n\n\nPress any key to exit");
+                Console.ReadKey();
+            }
+
+        }
+
+        public static void MyFavoriteNumber(out int favorite)
+        {
+            Console.WriteLine("What is your favorite number?");
+            int fav = int.Parse(Console.ReadLine());
+
+        }
+
+        public static void TimeStamp(ref string message)
+        {
+            message = $"{DateTime.Now} {message}";
+        }
+
+        public static void PrintMessage(string messagetoprint)
+        {
+            Console.WriteLine(messagetoprint);
+        }
+        public static string GetMessage()
+        {
+            Console.Write("Enter a message:  ");
+            string message = Console.ReadLine();
+            return message;
+        }
+    }
+    }
+}
